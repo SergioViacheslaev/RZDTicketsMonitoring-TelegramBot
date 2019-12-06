9a3ead2d12b174dc85f3f76c851a0a3bea9ddec2
SergioViacheslaev <internetwalker@yandex.ru>
2019-12-06 17:04:25 +0300

    Add Model classes

---
+++ src/main/java/ru/otus/rzdtelegrambot/model/Car.java
@@ -0,0 +1,23 @@
+package ru.otus.rzdtelegrambot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Вагон поезда
+ *
+ * @author Sergei Viacheslaev
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Car {
+    //типа вагона- Плацкарт, Купе, СВ
+    private String type;
+    private int freeSeats;
+    //минимальная цена для данного типа вагона
+    private int tariff;
+}
