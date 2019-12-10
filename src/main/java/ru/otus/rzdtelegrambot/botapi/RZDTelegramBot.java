a31c8f23ecfad39675fb46f578e56e9e9ab2b0bb
SergioViacheslaev <internetwalker@yandex.ru>
2019-12-10 14:15:54 +0300

    Add simple keyboard methods

--- src/main/java/ru/otus/rzdtelegrambot/botapi/RZDTelegramBot.java
+++ src/main/java/ru/otus/rzdtelegrambot/botapi/RZDTelegramBot.java
@@ -5,8 +5,14 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.otus.rzdtelegrambot.botconfig.RZDTelegramBotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RZDTelegramBot extends TelegramLongPollingBot {
     private RZDTelegramBotConfig botConfig;
@@ -22,6 +28,7 @@ public class RZDTelegramBot extends TelegramLongPollingBot {
 
         if (message != null && message.getText() != null) {
             sendMsg(message, String.format("echo: %s", message.getText()));
+            showMainMenu(message);
         }
 
     }
@@ -50,6 +57,67 @@ public class RZDTelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendMessage(SendMessage sendMessage) {
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void showMainMenu(final Message request) {
+
+        final ReplyKeyboardMarkup replyKeyboardMarkup = getMainMenuKeyboard();
+        final SendMessage msg =
+                createMessageWithKeyboard(request.getChatId().toString(), request.getMessageId(), replyKeyboardMarkup);
+
+
+        sendMessage(msg);
+    }
+
+    private ReplyKeyboardMarkup getMainMenuKeyboard() {
+
+        final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+
+        KeyboardRow row = new KeyboardRow();
+        row.add(new KeyboardButton("Найти поезда"));
+        row.add(new KeyboardButton("Помощь"));
+        keyboard.add(row);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        return replyKeyboardMarkup;
+    }
+
+    /**
+     * Отправка сообщения с клавиатурой и текстом
+     * сообщение отправляется с прикреплённым запросом пользователя
+     *
+     * @param chatId
+     * @param messageId
+     * @param replyKeyboardMarkup
+     * @return
+     */
+    private SendMessage createMessageWithKeyboard(final String chatId,
+                                                  final Integer messageId,
+                                                  final ReplyKeyboardMarkup replyKeyboardMarkup) {
+        final SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+//        sendMessage.setReplyToMessageId(messageId);
+        sendMessage.setText("Воспользуйтесь главным меню");
+        if (replyKeyboardMarkup != null) {
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        }
+        return sendMessage;
     }
 
 
